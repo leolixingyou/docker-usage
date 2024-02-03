bc9928fd2bcb9799aff5e3908b028fdcdca9cff8
leolixingyou <leesungwoo0501@naver.com>
2024-02-03 17:56:55 +0900

    docker_combination

--- start_container.sh
+++ start_container.sh
@@ -1,1 +1,6 @@
-docker start -i -a mobinha_xingyou
+docker_run_command=$(cat run_container.sh)
+
+docker_run_suffix=$(echo "$docker_run_command" | grep -oP "(?<=--name ).*"| awk '{print $1}')
+
+docker start -ai "$docker_run_suffix"
+
